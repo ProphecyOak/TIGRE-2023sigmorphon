872614b370fac45ff31e8615e2cd2ab6ae6bcf1e
ProphecyOak <drito610@gmail.com>
2024-03-04 11:24:03 -0500

    counterSorter fixed off by one and added header skip

--- src/counterSorter.py
+++ src/counterSorter.py
@@ -2,36 +2,37 @@ import sys, os, argparse
 from properties import*
 
 def writeLineTSV(w,l):
+    #print(l)
     w.write("\t".join(l)+"\n")
 
-def handleFile(sort, fileToSort, fileDest, column=0, sortFunction=lambda x,y: x[0], reverse=False):
+def handleFile(sort, fileToSort, fileDest, column=0, sortFunction=lambda x,y: x[0], reverse=False, skip=0):
     with open(fileToSort, encoding="UTF-8") as f:
-        lines = [x.split("\t") for x in f.read().strip().split("\n")]
+        lines = [x.split("\t") for x in f.read().strip().split("\n")[skip:]]
     lines.sort(key=lambda row: sortFunction(row,column),reverse=reverse)
     with open(fileDest, "w", encoding="UTF-8") as w:
         if sort:
             for x in lines: writeLineTSV(w,x)
             return
-        lineCounts = []
         curTotal = 0
-        curStat = None
+        curStat = lines[0][column]
         for x in lines:
             if x[column] == curStat:
                 curTotal += 1
             else:
-                writeLineTSV(w,[x[0],str(curTotal)])
+                writeLineTSV(w,[curStat,str(curTotal)])
                 curTotal = 1
                 curStat = x[column]
-        writeLineTSV(w,[x[0],str(curTotal)])
+        writeLineTSV(w,[curStat,str(curTotal)])
 
 def main(parsedArgs):
     counted = parsedArgs.operation in ["c","count"]
     handleFile(not counted,
                parsedArgs.file,
-               parsedArgs.dest if parsedArgs.dest != None else parsedArgs.file+f'.{["sorted","counted"][counted]}.tsv',
+               parsedArgs.dest if parsedArgs.dest != None else parsedArgs.file.replace(".tsv","")+f'.{["sorted","counted"][counted]}.tsv',
                parsedArgs.column,
                SORT_FUNCTIONS[parsedArgs.method],
-               parsedArgs.reverse)
+               parsedArgs.reverse,
+               parsedArgs.skip)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("errorCounter")
@@ -60,5 +61,11 @@ if __name__ == "__main__":
                         dest="reverse",
                         action="store_true",
                         help="Reverse the method of sorting.")
+    parser.add_argument("-s", "--skip",
+                        dest="skip",
+                        type=int,
+                        nargs="?",
+                        default=0,
+                        help="Skip first n lines of file")
 
     main(parser.parse_args())
