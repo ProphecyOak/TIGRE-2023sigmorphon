a386075efb5e49cd8d4c9c006be379677e8a1af4
ProphecyOak <drito610@gmail.com>
2024-03-06 16:21:59 -0500

    ERROR SUMMATION WORKS AHHAHAHAHAHAAH

--- src/errorSummation.py
+++ src/errorSummation.py
@@ -3,39 +3,33 @@ from properties import*
 
 
 def main(parsedArgs):
-    resultsByForm = {}
-    resultsByFeatures = {}
+    resultsByForm = []
     headers = ["lemma","features","gold"]
 
     ### Grabs lemmas from split file
     with open(parsedArgs.split, encoding="UTF-8") as s:
         for line in [x.split("\t") for x in s.read().strip().split("\n")]:
-            resultsByForm[line[2]] = line
-            resultsByFeatures[(line[0],line[1])] = line
+            resultsByForm.append(line)
 
     ### Grabs target values from supplied files.
-    numRelevantColumns = 3
-    for fileI in range(0,len(parsedArgs.files)//(numRelevantColumns+1)+numRelevantColumns,numRelevantColumns+1):
-        lemmaColumn, targetColumn, predictedColumn = int(parsedArgs.files[fileI+1]), int(parsedArgs.files[fileI+2]), int(parsedArgs.files[fileI+3])
+    for fileI in range(0,len(parsedArgs.files)//4+3,4):
+        targetColumn, predictedColumn, headerSkip = int(parsedArgs.files[fileI+1]), int(parsedArgs.files[fileI+2]), int(parsedArgs.files[fileI+3])
         headers.append(os.path.basename(parsedArgs.files[fileI]))
         with open(parsedArgs.files[fileI],encoding="UTF-8") as f:
             print(parsedArgs.files[fileI])
-            rows = [x.split("\t") for x in f.read().strip().split("\n")]
-        for row in rows:
-            if row[predictedColumn] == "craqueles": print(row)
-            if row[targetColumn] in resultsByForm.keys():
-                ### Adds row based on the target form
-                resultsByForm[row[targetColumn]].append(row[predictedColumn])
-            elif (row[lemmaColumn], row[targetColumn]) in resultsByFeatures.keys():
-                ### Adds row based on the features and lemma
-                resultsByFeatures[(row[lemmaColumn], row[targetColumn])].append(row[predictedColumn])
-            else: print(row)
+            rows = [x.split("\t") for x in f.read().strip().split("\n")][headerSkip:]
+        for row,resultRow in zip(rows,resultsByForm):
+            resultRow.append(row[predictedColumn] if row[predictedColumn] not in [resultRow[1],resultRow[2]] else "")
         
     ### Writes all of the rows to the output file
     with open(parsedArgs.output, "w", encoding="UTF-8")  as w:
         w.write("\t".join([str(x) for x in headers])+"\n")
-        for row in resultsByForm.values():
-            if len(row) > 3: w.write("\t".join([str(x) for x in row])+"\n")
+        for row in resultsByForm:
+            if not parsedArgs.includeCorrect:
+                rowCheck = list(filter("".__ne__, row))
+                if len(rowCheck) > 3: w.write("\t".join([str(x) for x in row])+"\n")
+            else:
+                w.write("\t".join([str(x) for x in row])+"\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("errorSummation.py")
@@ -48,21 +42,13 @@ if __name__ == "__main__":
                         nargs="?",
                         default=os.path.join(SHARED_TASK_DATA_FOLDER,"fra.dev"),
                         help="split to draw rows from.")
-    parser.add_argument("-lc","--lemmaColumn",
-                        dest="lemmaColumn",
-                        type=int,
-                        nargs="?",
-                        default=0,
-                        help="Column in split with lemmas.")
-    parser.add_argument("-fc","--featureColumn",
-                        dest="featureColumn",
-                        type=int,
-                        nargs="?",
-                        default=1,
-                        help="Column in split with features.")
     parser.add_argument("-o","--output",
                         dest="output",
                         nargs="?",
                         default="../CounterSorterOutput/summedErrors.tsv",
                         help="Output file.")
+    parser.add_argument("-c","--correct",
+                        dest="includeCorrect",
+                        action="store_true",
+                        help="Includes correct rows as well.")
     main(parser.parse_args())
--- src/formatNeuralErrors.py
+++ src/formatNeuralErrors.py
@@ -3,44 +3,25 @@ import re
 import argparse
 from properties import *
 
-def sortOutErrors(test, language, outputFolder, dataFolder, errorFolder):
+def sortOutErrors(test, language, outputFolder, errorFolder):
     fileToCheck = "test" if test else "dev"
     with open(f"{outputFolder}/{language}.decode.{fileToCheck}.tsv") as f:
         contents = f.read()
 
     lines = contents.strip().split("\n")
-    columns = []
+    rows = []
     for line in lines[1:]:
-        columns.append(line.replace(" ","").split("\t"))
+        rows.append(line.replace(" ","").split("\t"))
 
-    errorColumns = list(filter(lambda x: x[3] != '0', columns))
-    if fileToCheck == "test": fileToCheck = "tst"  #Because for whatever reason, neural_transducer doesnt use same extension formatting :/
-    with (open(f"{dataFolder}/{language}.{fileToCheck}") as dat,
-          open(f"{errorFolder}/errors.{language}.{fileToCheck}.tsv","w") as e,
-          open(f"{errorFolder}/unexpectedForms.{language}.{fileToCheck}.tsv","w") as u):
-        lemmaDict = {}
-        e.write("lemma\t"+lines[0]+"\n")
-        u.write(lines[0]+"\n")
-        for form in dat.read().strip().split("\n"):
-            splitForm = re.split(r"\s+",form)
-            lemmaDict[splitForm[2]] = splitForm[0]
-        reducedErrorColumns = []
-        for line in errorColumns:
-            try:
-                line.insert(0,lemmaDict[line[1]])
-                reducedErrorColumns.append(line)
-            except:
-                u.write("\t".join(line)+"\n")
-        reducedErrorColumns.sort(key=lambda x: x[0])
-        for line in reducedErrorColumns:
-            if len(line) == 5: e.write("\t".join(line)+"\n")
+    with open(f"{errorFolder}/errors.{language}.{fileToCheck}.tsv","w") as e:
+          for row in rows: e.write("\t".join(row)+"\n")
 
 def main(parsedArgs):
     if parsedArgs.all:
         #Loop through languages like nonneural.py does
         print("This option is not yet implemented")
     else:
-        sortOutErrors(parsedArgs.test, parsedArgs.lang, parsedArgs.path, parsedArgs.original, parsedArgs.dest)
+        sortOutErrors(parsedArgs.test, parsedArgs.lang, parsedArgs.path, parsedArgs.dest)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("catalogNeuralErrors.py")
@@ -54,11 +35,6 @@ if __name__ == "__main__":
                         nargs="?",
                         default=NEURAL_ERRORS_FOLDER,
                         help="Path to destination folder for error catalog.")
-    parser.add_argument("-o","--original",
-                        dest="original",
-                        nargs="?",
-                        default=SHARED_TASK_DATA_FOLDER,
-                        help="Path to folder with original splits to grab lemmas from.")
     parser.add_argument("-t","--test",
                         dest="test",
                         action="store_true",
