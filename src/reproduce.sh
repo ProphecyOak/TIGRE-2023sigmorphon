698dd0ffbf378312efaabb31d5fd2ff8374ad2dc
ProphecyOak <drito610@gmail.com>
2024-03-07 10:20:22 -0500

    But its good now

--- src/reproduce.sh
+++ src/reproduce.sh
@@ -11,24 +11,20 @@ python nonneural.py -o -t -p ../SegmentationsSplits
 
 python errorSummation.py ../SharedTaskData/fra.dev.out 2 0 \
     ../NeuralTransducerFormatted/fra.neural.dev.tsv 0 0 \
-    -s ../SharedTaskData/fra.dev
-mv ../CounterSorterOutput/summedErrors.tsv \
-    ../CounterSorterOutput/summed_Original-Dev.tsv
+    -s ../SharedTaskData/fra.dev \
+    -o ../CounterSorterOutput/summed_Original-Dev.tsv
     
 python errorSummation.py ../SharedTaskData/fra.tst.out 2 0 \
     ../NeuralTransducerFormatted/fra.neural.test.tsv 0 0 \
-    -s ../SharedTaskData/fra.tst
-mv ../CounterSorterOutput/summedErrors.tsv \
-    ../CounterSorterOutput/summed_Original-Tst.tsv
+    -s ../SharedTaskData/fra.tst \
+    -o ../CounterSorterOutput/summed_Original-Tst.tsv
 
 python errorSummation.py ../SegmentationsSplits/fra.dev.out 2 0 \
     ../NeuralTransducerFormatted/fra-segmentations.neural.dev.tsv 0 0 \
-    -s ../SegmentationsSplits/fra.dev
-mv ../CounterSorterOutput/summedErrors.tsv \
-    ../CounterSorterOutput/summed_Segmentations-Dev.tsv
+    -s ../SegmentationsSplits/fra.dev \
+    -o ../CounterSorterOutput/summed_Segmentations-Dev.tsv
 
 python errorSummation.py ../SegmentationsSplits/fra.tst.out 2 0 \
     ../NeuralTransducerFormatted/fra-segmentations.neural.test.tsv 0 0 \
-    -s ../SegmentationsSplits/fra.tst
-mv ../CounterSorterOutput/summedErrors.tsv \
-    ../CounterSorterOutput/summed_Segmentations-Tst.tsv
+    -s ../SegmentationsSplits/fra.tst \
+    -o ../CounterSorterOutput/summed_Segmentations-Tst.tsv
